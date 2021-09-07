9be5e4cf555aef1c75de17578dc4541e7c15f1b8
Alexis R <alexisr@Alexiss-MacBook-Air.local>
2021-09-07 18:26:47 -0500

    actualizacion

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -4,7 +4,7 @@ export default class Buscador extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar text-dark navbar-expand-lg navbar-light bg-light ">
+                <nav className="navbar navbar-expand-lg navbar-light  navbar-expand-sm bg-black">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="#"><img src="https://res.cloudinary.com/dd01p9lb9/image/upload/v1630871290/api_sprint2/logo_vlh6uj.png" width="65px"/></a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,17 +14,17 @@ export default class Buscador extends Component {
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             
                                     <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Todas</a>
+                                    <a className="nav-link active text-white h5" aria-current="page" href="#">Todas</a>
                                     </li>
                                     <li className="nav-item">
-                                    <a className="nav-link" href="#">Mas valoradas</a>
+                                    <a className="nav-link text-white h5" href="#">Mas valoradas</a>
                                     </li>
                                     <li className="nav-item">
-                                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Menos valoradas</a>
+                                    <a className="nav-link disabled text-white h5" href="#" tabindex="-1" aria-disabled="true">Menos valoradas</a>
                                     </li>
                             </ul>
                                 <form className="d-flex">
-                                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                    <input className="form-control " width="300px" type="search" placeholder="Search" aria-label="Search" />
                                     <button className="btn btn-outline-success" type="submit">Search</button>
                                 </form>
                         </div>
